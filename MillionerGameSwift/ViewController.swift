5ffc40ed4ae6a23b0f7e32643881b0917351a884
Alexander Mink <alexander.mink1@gmail.com>
2020-09-24 15:55:05 +0300

    c4 template

---
+++ MillionerGameSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MillionerGameSwift
+//
+//  Created by Александр Минк on 24.09.2020.
+//  Copyright © 2020 Alexander Mink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
